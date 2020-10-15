5085b59f493e915c3f9203d9828d89806888b02a
Ophélie FOUCAULT <foucault.ophelie.pro@gmail.com>
2020-10-15 17:30:31 +0200

    Camera et personnage qui tourne avec la souris

---
+++ Assets/Scripts/3C/CameraMove.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMove : MonoBehaviour
+{
+    public GameObject m_camera;
+    public float speed;
+    public float maxY;
+    public float minY;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        transform.Rotate(0, Input.GetAxisRaw("Mouse X") * Time.deltaTime * speed, 0);
+        m_camera.transform.Rotate(Input.GetAxisRaw("Mouse Y") * Time.deltaTime * -speed, 0, 0);
+
+
+        if (m_camera.transform.localRotation.x < minY)
+        {
+            Debug.Log("trop haut");
+            m_camera.transform.localRotation = new Quaternion(minY, m_camera.transform.localRotation.y, m_camera.transform.localRotation.z, m_camera.transform.localRotation.w);
+        }
+
+        if (m_camera.transform.localRotation.x > maxY)
+        {
+            Debug.Log("trop bas");
+            m_camera.transform.localRotation = new Quaternion(maxY, m_camera.transform.localRotation.y, m_camera.transform.localRotation.z, m_camera.transform.localRotation.w);
+        }
+    }
+}

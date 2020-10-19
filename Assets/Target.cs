95af26035641fee30de721f1e2ad3227300e2138
wlid <thomas.jorda@insa-lyon.fr>
2020-10-19 15:38:01 +0200

    begining of shoot mechanics

---
+++ Assets/Target.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+public class Target : MonoBehaviour
+{
+    public int multiplier = 1;
+    public int score = 100;
+    public bool hit = false;
+
+    public void takeAShot()
+    {
+        hit = true;
+        if (hit)
+        {
+            Destroy(gameObject);
+        }
+    }
+}

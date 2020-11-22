59fa5e8d850b7fcb3db8e935b4039b81fa72d33f
wlid <thomas.jorda@insa-lyon.fr>
2020-11-22 13:41:01 +0100

    First draft of Tutorial

---
+++ Assets/Scripts/PopupSystem.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class PopupSystem : MonoBehaviour
+{
+    public GameObject popUpBox;
+    //public Animator animator;
+    public TMP_Text popUpText;
+
+    public void openPopUp(string text)
+    {
+        popUpBox.SetActive(true);
+        popUpText.text = text;
+        //animator.SetTrigger("pop");
+    }
+}
